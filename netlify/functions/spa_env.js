5b9fa039fe4f9d8219eeed5255cda727dccd7fa0
radu86d-cpu <radu86d@gmail.com>
2025-08-25 09:12:13 +0300

    Update spa_env.js

--- netlify/functions/spa_env.js
+++ netlify/functions/spa_env.js
@@ -1,17 +1,3 @@
-// netlify/functions/spa_env.js
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-store'
-    },
-    body: JSON.stringify({
-      SUPABASE_URL: process.env.SUPABASE_URL,
-      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
-    })
-  };
-};
 import { cors, json, method } from "./_shared/utils.js";
 
 export default async (req) => {
