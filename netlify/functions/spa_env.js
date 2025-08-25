b3d7eb892f3f9ddd7f36583756f800a401d1535b
radu86d-cpu <radu86d@gmail.com>
2025-08-25 09:11:05 +0300

    Update spa_env.js

--- netlify/functions/spa_env.js
+++ netlify/functions/spa_env.js
@@ -12,3 +12,14 @@ exports.handler = async () => {
     })
   };
 };
+import { cors, json, method } from "./_shared/utils.js";
+
+export default async (req) => {
+  const m = method(req, ["GET"]);
+  const headers = cors(req);
+  if (m === "OPTIONS") return new Response(null, { status: 204, headers });
+  return json({
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+  }, { headers });
+};
